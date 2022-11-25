7b47bb984c1e2a11c4d2c173b0332a0c0c02e42e
JoshuaW13 <josh.walters@shaw.ca>
2022-11-25 14:40:40 -0700

    fixed mobile

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,3 +1,13 @@
 function set_grid_item_size(){
     let viewport_size = window.innerWidth;
+    let grid = document.querySelector(".projects");
+    if(viewport_size<= 550){
+        grid.style.gridTemplateColumns = "repeat(auto-fit, minmax(200px, 1fr))"
+    }
+    else{
+        grid.style.gridTemplateColumns = "repeat(auto-fit, minmax(500px, 1fr))"
+    }
 }
+
+window.addEventListener("resize",set_grid_item_size);
+set_grid_item_size();
